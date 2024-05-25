3aaba0974a94b351c4ff89f1a54bcd8fb04af96e
bonn <bonnbonito@gmail.com>
2024-05-25 08:46:13 +0800

    removed add brevo switch

--- inc/classes/Brevo.php
+++ inc/classes/Brevo.php
@@ -30,6 +30,46 @@ class Brevo {
 		add_action( 'nova_user_partner_approved', array( $this, 'add_to_brevo_list' ) );
 	}
 
+	public function check_user_exists( $email ) {
+
+		$curl = curl_init();
+
+		curl_setopt_array(
+			$curl,
+			array(
+				CURLOPT_URL            => 'https://api.brevo.com/v3/contacts/' . urlencode( $email ),
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING       => '',
+				CURLOPT_MAXREDIRS      => 10,
+				CURLOPT_TIMEOUT        => 30,
+				CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+				CURLOPT_CUSTOMREQUEST  => 'GET',
+				CURLOPT_HTTPHEADER     => array(
+					'accept: application/json',
+					'api-key: ' . $this->brevo_api,
+				),
+			)
+		);
+
+		$response  = curl_exec( $curl );
+		$http_code = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
+		$err       = curl_error( $curl );
+
+		curl_close( $curl );
+
+		$logger = new \WC_Logger();
+
+		if ( $err ) {
+			$logger->error( 'cURL Error #: ' . $err . ' for checking user email: ' . array( 'source' => 'brevo_api' ) );
+		}
+
+		if ( $http_code == 200 ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	public function add_to_brevo_list( $user ) {
 
 		if ( ! $this->brevo_api ) {
--- inc/helpers/autoloader.php
+++ inc/helpers/autoloader.php
@@ -29,5 +29,7 @@ require NOVA_DIR_PATH . '/inc/classes/Nova_Quote.php';
 require NOVA_DIR_PATH . '/inc/classes/Shortcodes.php';
 require NOVA_DIR_PATH . '/inc/classes/Pending_Payment.php';
 require NOVA_DIR_PATH . '/inc/classes/Zoho.php';
-require NOVA_DIR_PATH . '/inc/classes/Brevo.php';
+if ( get_field( 'brevo_integration', 'option' ) ) {
+	require NOVA_DIR_PATH . '/inc/classes/Brevo.php';
+}
 // require NOVA_DIR_PATH . '/inc/classes/GoogleSheets.php';
