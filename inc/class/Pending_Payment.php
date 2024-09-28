9962138cc0a8e5eb09f05b0d5aa555848e2b3eae
bonn <bonnbonito@gmail.com>
2024-09-28 12:42:32 +0800

    checkbox overdue

--- inc/class/Pending_Payment.php
+++ inc/class/Pending_Payment.php
@@ -555,8 +555,6 @@ class Pending_Payment {
 
 		if ( strtotime( $today ) > strtotime( $payment_date ) ) {
 			update_post_meta( $payment_order_id, 'is_overdue', true );
-		} else {
-			update_post_meta( $payment_order_id, 'is_overdue', false );
 		}
 	}
 
