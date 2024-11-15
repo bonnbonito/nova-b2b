70e1ac0d573fb8ee8ae294b90b49be38c741c84e
bonn <bonnbonito@gmail.com>
2024-11-16 06:26:36 +0800

    pending payment reminder email

--- inc/class/Pending_Payment.php
+++ inc/class/Pending_Payment.php
@@ -729,7 +729,10 @@ class Pending_Payment {
 					$days_later = strtotime( $shipped_date . ' +' . intval( $days ) . ' days' );
 					$date_later = date( 'F d, Y', $days_later );
 
-					if ( $today == $date_later ) {
+					$is_last_row = get_row_index() == count( get_field( 'payment_emails', $payment_type ) );
+					$email_sent  = get_post_meta( $payment_order_id, 'payment_email_key_' . get_row_index(), true );
+
+					if ( $today == $date_later || ( $is_last_row && $today >= $date_later && ! $email_sent ) ) {
 
 						$subject = get_sub_field( 'subject' );
 						$subject = str_replace( '{customer_name}', $first_name, $subject );
