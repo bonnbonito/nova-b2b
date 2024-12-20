fef5909f3a0059dbd2d5cc2558c8697126ee375a
Bonn Joel Elimanco <bonnbonito@gmail.com>
2024-12-20 08:31:22 +0800

    feat: able to select images in dropbox gallery

--- inc/class/Dropbox.php
+++ inc/class/Dropbox.php
@@ -34,6 +34,7 @@ class Dropbox {
 		add_action( 'wp_ajax_save_fetched_images', array( $this, 'save_fetched_images' ) );
 		add_shortcode( 'nova_dropbox_gallery', array( $this, 'display_dropbox_gallery' ) );
 		add_filter( 'kadence_classic_meta_box_post_types', array( $this, 'remove_post_types' ) );
+		add_action( 'save_post', array( $this, 'save_selected_images' ), 10, 2 );
 	}
 
 	public function remove_post_types( $post_types ) {
@@ -62,7 +63,7 @@ class Dropbox {
 
 		$id = $atts['id'];
 
-		$fetched_images = get_post_meta( $id, 'gallery_images', true );
+		$fetched_images = get_post_meta( $id, 'selected_fetched_images', true ) ?: get_post_meta( $id, 'gallery_images', true );
 
 		if ( empty( $fetched_images ) ) {
 			return '';
@@ -73,48 +74,48 @@ class Dropbox {
 
 		ob_start();
 		?>
-<section id="dropbox-<?php echo esc_attr( $id ); ?>" class="embla nova-dropbox-gallery theme-dark">
-    <div class="embla__viewport">
-        <div class="embla__container">
-            <?php foreach ( $fetched_images as $image ) : ?>
-            <div class="embla__slide">
-                <div class="embla__lazy-load">
-                    <span class="embla__lazy-load__spinner"></span>
-                    <a href="<?php echo esc_url( $image['sharedLink'] ); ?>">
-                        <img class="embla__slide__img embla__lazy-load__img"
-                            src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D" alt="nova image"
-                            data-src="<?php echo esc_url( $image['sharedLink'] ); ?>" />
-                    </a>
-                </div>
-            </div>
-            <?php endforeach; ?>
-        </div>
-    </div>
+		<section id="dropbox-<?php echo esc_attr( $id ); ?>" class="embla nova-dropbox-gallery theme-dark">
+			<div class="embla__viewport">
+				<div class="embla__container">
+					<?php foreach ( $fetched_images as $image ) : ?>
+						<div class="embla__slide">
+							<div class="embla__lazy-load">
+								<span class="embla__lazy-load__spinner"></span>
+								<a href="<?php echo esc_url( $image['sharedLink'] ); ?>">
+									<img class="embla__slide__img embla__lazy-load__img"
+										src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D" alt="nova image"
+										data-src="<?php echo esc_url( $image['sharedLink'] ); ?>" />
+								</a>
+							</div>
+						</div>
+					<?php endforeach; ?>
+				</div>
+			</div>
 
-    <div class="embla__controls">
-        <div class="embla__buttons">
-            <button class="embla__button embla__button--prev" type="button">
-                <svg class="embla__button__svg" viewBox="0 0 532 532">
-                    <path fill="currentColor"
-                        d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z">
-                    </path>
-                </svg>
-            </button>
+			<div class="embla__controls">
+				<div class="embla__buttons">
+					<button class="embla__button embla__button--prev" type="button">
+						<svg class="embla__button__svg" viewBox="0 0 532 532">
+							<path fill="currentColor"
+								d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.796-36.208 13.796-50.002 0a5994246.277 5994246.277 0 0 0-229.332-229.454 35.065 35.065 0 0 1-10.326-25.126c0-9.2 3.393-18.26 10.326-25.2C172.192 194.973 332.731 34.31 355.66 11.354Z">
+							</path>
+						</svg>
+					</button>
 
-            <button class="embla__button embla__button--next" type="button">
-                <svg class="embla__button__svg" viewBox="0 0 532 532">
-                    <path fill="currentColor"
-                        d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z">
-                    </path>
-                </svg>
-            </button>
-        </div>
+					<button class="embla__button embla__button--next" type="button">
+						<svg class="embla__button__svg" viewBox="0 0 532 532">
+							<path fill="currentColor"
+								d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.796 36.208-13.796 50.002 0 22.928 22.947 206.395 206.507 229.332 229.454a35.065 35.065 0 0 1 10.326 25.126c0 9.2-3.393 18.26-10.326 25.2-45.865 45.901-206.404 206.564-229.332 229.52Z">
+							</path>
+						</svg>
+					</button>
+				</div>
 
-        <div class="embla__dots"></div>
-    </div>
-</section>
+				<div class="embla__dots"></div>
+			</div>
+		</section>
 
-<?php
+		<?php
 		$output = ob_get_clean();
 
 		return $output;
@@ -124,7 +125,7 @@ class Dropbox {
 
 		// Get the post ID and images from the AJAX request
 		$post_id = intval( $_POST['post_id'] );
-		$images  = isset( $_POST['images'] ) ? json_decode( stripslashes( $_POST['images'] ), true ) : array();
+		$images = isset( $_POST['images'] ) ? json_decode( stripslashes( $_POST['images'] ), true ) : array();
 
 		if ( empty( $post_id ) || empty( $images ) ) {
 			wp_send_json_error( array( 'error' => 'Invalid post ID or images' ) );
@@ -180,6 +181,39 @@ class Dropbox {
 		);
 	}
 
+
+	public function save_selected_images( $post_id, $post ) {
+		if ( $post->post_type !== self::GALLERY ) {
+			return;
+		}
+
+		if ( ! isset( $_POST['dropbox_gallery_nonce'] ) || ! wp_verify_nonce( $_POST['dropbox_gallery_nonce'], 'save_dropbox_gallery' ) ) {
+			return;
+		}
+
+		// Check if the user has permission to save meta for this post.
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+
+		// Check if this is an autosave. If so, return.
+		if ( wp_is_post_autosave( $post_id ) || wp_is_post_revision( $post_id ) ) {
+			return;
+		}
+
+		// Save the selected fetched images.
+		if ( isset( $_POST['selected_fetched_images'] ) && is_array( $_POST['selected_fetched_images'] ) ) {
+			$selected_images = array_map( function ($url) {
+				return array( 'sharedLink' => esc_url_raw( $url ) );
+			}, $_POST['selected_fetched_images'] );
+
+			update_post_meta( $post_id, 'selected_fetched_images', $selected_images );
+		} else {
+			// If no images are selected, delete the meta key.
+			delete_post_meta( $post_id, 'selected_fetched_images' );
+		}
+	}
+
 	public function dropbox_gallery_metabox_callback( $post ) {
 		global $pagenow;
 		// Add a nonce field so we can check for it later.
@@ -188,37 +222,56 @@ class Dropbox {
 		$fetched_images = get_post_meta( $post->ID, 'gallery_images', true );
 
 		$dropbox_folder = get_post_meta( $post->ID, 'dropbox_folder', true );
-		?>
-<h4>[nova_dropbox_gallery id="<?php echo esc_attr( $post->ID ); ?>"]</h4>
-<label for="dropbox_folder">
-    <?php _e( 'Dropbox Folder', 'nova-b2b' ); ?>
-</label>
-<input type="text" id="dropbox_folder" name="dropbox_folder" value="<?php echo esc_attr( $dropbox_folder ); ?>" />
-<p class="description">
-    <?php _e( 'Enter the path of the Dropbox folder containing the images for this gallery.', 'nova-b2b' ); ?>
-</p>
-<p></p>
 
-<?php
+		$selected_fetched_images = get_post_meta( $post->ID, 'selected_fetched_images', true );
+
+
+		$checked = '';
+		?>
+		<h4>[nova_dropbox_gallery id="<?php echo esc_attr( $post->ID ); ?>"]</h4>
+		<label for="dropbox_folder">
+			<?php _e( 'Dropbox Folder', 'nova-b2b' ); ?>
+		</label>
+		<input type="text" id="dropbox_folder" name="dropbox_folder" value="<?php echo esc_attr( $dropbox_folder ); ?>" />
+		<p class="description">
+			<?php _e( 'Enter the path of the Dropbox folder containing the images for this gallery.', 'nova-b2b' ); ?>
+		</p>
+		<p></p>
+
+		<?php
 		if ( $pagenow !== 'post-new.php' ) :
 			?>
-<div class="flex" id="divActions">
-    <button type="button" class="button button-primary nova-dropbox-gallery-update" id="dropboxFetchImages">
-        <?php _e( 'Fetch Images', 'nova-b2b' ); ?>
-    </button>
-    <div class="spinner" role="status"></div>
-</div>
-<div id="fetched-images">
-    <?php if ( ! empty( $fetched_images ) ) : ?>
-    <?php foreach ( $fetched_images as $image ) : ?>
-    <img src="<?php echo esc_url( $image['sharedLink'] ); ?>" alt="">
-    <?php endforeach; ?>
-    <?php else : ?>
-    <h4>No fetched images</h4>
-    <?php endif; ?>
-</div>
-<?php
-endif;
+			<div class="flex" id="divActions">
+				<button type="button" class="button button-primary nova-dropbox-gallery-update" id="dropboxFetchImages">
+					<?php _e( 'Fetch Images', 'nova-b2b' ); ?>
+				</button>
+				<div class="spinner" role="status"></div>
+			</div>
+			<div id="fetched-images">
+				<?php if ( ! empty( $fetched_images ) ) : ?>
+					<?php foreach ( $fetched_images as $image ) : ?>
+						<?php if ( ! isset( $image['sharedLink'] ) || ! $image['sharedLink'] ) {
+							continue;
+						}
+
+						if ( $selected_fetched_images ) {
+							$checked = in_array( $image['sharedLink'], array_column( $selected_fetched_images, 'sharedLink' ), true ) ? 'checked' : '';
+						}
+
+						?>
+						<label>
+							<input type="checkbox" name="selected_fetched_images[]" value="<?php echo esc_attr( $image['sharedLink'] ); ?>"
+								<?php echo $checked; ?>>
+							<img src="<?php echo esc_url( $image['sharedLink'] ); ?>" alt="">
+						</label>
+					<?php endforeach; ?>
+				<?php else : ?>
+					<h4>No fetched images</h4>
+				<?php endif; ?>
+			</div>
+			<?php
+			$selected_fetched_images = get_post_meta( $post->ID, 'selected_fetched_images', true );
+		endif;
 	}
 
 
@@ -238,13 +291,13 @@ endif;
 			'admin-dropbox-gallery',
 			'NovaDropboxGallery',
 			array(
-				'ajax_url'              => admin_url( 'admin-ajax.php' ),
-				'nonce'                 => wp_create_nonce( 'nova-dropbox-gallery' ),
-				'dropbox_app_key'       => get_field( 'dropbox_app_key', 'option' ),
-				'dropbox_secret'        => get_field( 'dropbox_secret_key', 'option' ),
-				'dropbox_token'         => get_field( 'dropbox_token_access', 'option' ),
+				'ajax_url' => admin_url( 'admin-ajax.php' ),
+				'nonce' => wp_create_nonce( 'nova-dropbox-gallery' ),
+				'dropbox_app_key' => get_field( 'dropbox_app_key', 'option' ),
+				'dropbox_secret' => get_field( 'dropbox_secret_key', 'option' ),
+				'dropbox_token' => get_field( 'dropbox_token_access', 'option' ),
 				'dropbox_refresh_token' => get_field( 'dropbox_refresh_token', 'option' ),
-				'post_id'               => $post->ID,
+				'post_id' => $post->ID,
 			)
 		);
 	}
@@ -288,7 +341,7 @@ endif;
 			$url,
 			array(
 				'headers' => $headers,
-				'body'    => $post_data,
+				'body' => $post_data,
 			)
 		);
 
@@ -332,7 +385,7 @@ endif;
 
 		$post_data = json_encode(
 			array(
-				'path'     => $file_path,
+				'path' => $file_path,
 				'settings' => array(
 					'requested_visibility' => 'public',
 				),
@@ -343,7 +396,7 @@ endif;
 			$url,
 			array(
 				'headers' => $headers,
-				'body'    => $post_data,
+				'body' => $post_data,
 			)
 		);
 
@@ -362,22 +415,22 @@ endif;
 	}
 
 	public function get_refresh_token() {
-		$client_id     = get_field( 'dropbox_app_key', 'option' );
+		$client_id = get_field( 'dropbox_app_key', 'option' );
 		$client_secret = get_field( 'dropbox_secret_key', 'option' );
 		$refresh_token = get_field( 'dropbox_refresh_token', 'option' );
 
-		$url    = 'https://api.dropboxapi.com/oauth2/token';
+		$url = 'https://api.dropboxapi.com/oauth2/token';
 		$params = array(
-			'grant_type'    => 'refresh_token',
+			'grant_type' => 'refresh_token',
 			'refresh_token' => $refresh_token,
-			'client_id'     => $client_id,
+			'client_id' => $client_id,
 			'client_secret' => $client_secret,
 		);
 
 		$response = wp_remote_post(
 			$url,
 			array(
-				'body'    => $params,
+				'body' => $params,
 				'headers' => array(
 					'Content-Type' => 'application/x-www-form-urlencoded',
 				),
@@ -406,11 +459,11 @@ endif;
 			return false; // indicate failure
 		}
 
-		$move_url    = 'https://api.dropboxapi.com/2/files/move_v2';
+		$move_url = 'https://api.dropboxapi.com/2/files/move_v2';
 		$move_params = json_encode(
 			array(
-				'from_path'  => $old_path,
-				'to_path'    => $new_path,
+				'from_path' => $old_path,
+				'to_path' => $new_path,
 				'autorename' => true,
 			)
 		);
@@ -418,11 +471,11 @@ endif;
 		$response = wp_remote_post(
 			$move_url,
 			array(
-				'method'  => 'POST',
-				'body'    => $move_params,
+				'method' => 'POST',
+				'body' => $move_params,
 				'headers' => array(
 					'Authorization' => 'Bearer ' . $access_token,
-					'Content-Type'  => 'application/json',
+					'Content-Type' => 'application/json',
 				),
 			)
 		);
@@ -433,7 +486,7 @@ endif;
 		}
 
 		$response_body = wp_remote_retrieve_body( $response );
-		$move_data     = json_decode( $response_body, true );
+		$move_data = json_decode( $response_body, true );
 
 		if ( isset( $move_data['metadata'] ) ) {
 			// Folder moved successfully
@@ -445,18 +498,18 @@ endif;
 	}
 
 	public function get_access_token() {
-		$clientId     = get_field( 'dropbox_app_key', 'option' );
+		$clientId = get_field( 'dropbox_app_key', 'option' );
 		$clientSecret = get_field( 'dropbox_secret_key', 'option' );
-		$redirectUri  = get_field( 'dropbox_redirect_url', 'option' );
-		$url          = 'https://api.dropboxapi.com/oauth2/token';
-		$params       = array(
-			'code'          => $_GET['code'],
-			'grant_type'    => 'authorization_code',
-			'client_id'     => $clientId,
+		$redirectUri = get_field( 'dropbox_redirect_url', 'option' );
+		$url = 'https://api.dropboxapi.com/oauth2/token';
+		$params = array(
+			'code' => $_GET['code'],
+			'grant_type' => 'authorization_code',
+			'client_id' => $clientId,
 			'client_secret' => $clientSecret,
-			'redirect_uri'  => $redirectUri,
+			'redirect_uri' => $redirectUri,
 		);
-		$response     = wp_remote_post( $url, array( 'body' => $params ) );
+		$response = wp_remote_post( $url, array( 'body' => $params ) );
 		if ( is_wp_error( $response ) ) {
 			// Handle error
 			return null;
@@ -465,24 +518,24 @@ endif;
 		$data = $body ? json_decode( $body, true ) : null;
 
 		return array(
-			'access_token'  => $data['access_token'] ?? null,
+			'access_token' => $data['access_token'] ?? null,
 			'refresh_token' => $data['refresh_token'] ?? null,
 		);
 	}
 
 	public function exchangeAuthorizationCodeForAccessToken( $authorizationCode ) {
-		$clientId     = get_field( 'dropbox_app_key', 'option' );
+		$clientId = get_field( 'dropbox_app_key', 'option' );
 		$clientSecret = get_field( 'dropbox_secret_key', 'option' );
-		$redirectUri  = get_field( 'dropbox_redirect_url', 'option' );
-		$url          = 'https://api.dropboxapi.com/oauth2/token';
-		$params       = array(
-			'code'          => $authorizationCode,
-			'grant_type'    => 'authorization_code',
-			'client_id'     => $clientId,
+		$redirectUri = get_field( 'dropbox_redirect_url', 'option' );
+		$url = 'https://api.dropboxapi.com/oauth2/token';
+		$params = array(
+			'code' => $authorizationCode,
+			'grant_type' => 'authorization_code',
+			'client_id' => $clientId,
 			'client_secret' => $clientSecret,
-			'redirect_uri'  => $redirectUri,
+			'redirect_uri' => $redirectUri,
 		);
-		$response     = wp_remote_post( $url, array( 'body' => $params ) );
+		$response = wp_remote_post( $url, array( 'body' => $params ) );
 		if ( is_wp_error( $response ) ) {
 			// Handle error
 			return null;
@@ -490,7 +543,7 @@ endif;
 		$body = wp_remote_retrieve_body( $response );
 		$data = $body ? json_decode( $body, true ) : null;
 		return array(
-			'access_token'  => $data['access_token'] ?? null,
+			'access_token' => $data['access_token'] ?? null,
 			'refresh_token' => $data['refresh_token'] ?? null,
 		);
 	}
