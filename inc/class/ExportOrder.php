cb4f7b4db4dd5004c1faad4672d35517ff9cf99d
Bonn Joel Elimanco <bonnbonito@gmail.com>
2025-02-06 06:11:07 +0800

    change month format

--- inc/class/ExportOrder.php
+++ inc/class/ExportOrder.php
@@ -158,7 +158,7 @@ class ExportOrder {
 						'Currency' => wp_strip_all_tags( $order->get_currency() ),
 						'Item Price' => floatval( $item_total ),
 						'Total Price' => floatval( $order->get_total() ),
-						'Order Date' => wp_strip_all_tags( $order->get_date_created()->format( 'M/d/Y' ) ),
+						'Order Date' => wp_strip_all_tags( $order->get_date_created()->format( 'm/d/Y' ) ),
 						'Payment Type' => wp_strip_all_tags( $payment_type ),
 					);
 				}
